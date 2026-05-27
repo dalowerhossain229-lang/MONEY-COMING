aab5b626b0d46c8b803210a0ab2c3d18f1a95965
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-05-27 22:47:32 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // 🎰 [উইনগো কালার ট্রেড ওরিজিনাল ডোমেইন সিঙ্ক]
-const MAIN_SITE_URL = "https://onrender.com"; 
+const MAIN_SITE_URL = "https://betlover247.onrender.com"; 
 
 // 💰 ১. লাইভ অ্যাকাউন্ট ব্যালেন্স নিয়ে আসার ডেডিকেটেড গেটওয়ে
 app.get('/api/moneycoming-balance', async (req, res) => {
