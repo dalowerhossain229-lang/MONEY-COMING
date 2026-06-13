4eff548776ab62c26fd66a85469d7d512fb617da
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-06-13 10:04:26 +0600

    Update server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ io.on('connection', (socket) => {
 });
 
 // 🌐 [🔒 ওস্তাদ! রেন্ডার হোস্টিং এবং টার্মাক্সের জন্য পোর্ট ৯৯৯৯ কড়া কিংস বর্ম লক ফায়ার 🔒]
-const PORT = process.env.PORT || 34000;
+const PORT = process.env.PORT || 9999;
 server.listen(PORT, () => {
     console.log(`🎰 VIP JILI Money Coming Casino Engine Running on port 9999`);
 });
